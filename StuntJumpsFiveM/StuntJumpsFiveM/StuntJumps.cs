b2d18c996261a7c4af95dd1f3c52bcb2ab4dfa8a
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-10-02 10:35:59 +0200

    Add events to enable/disable blips and stunt jumps

--- StuntJumpsFiveM/StuntJumpsFiveM/StuntJumps.cs
+++ StuntJumpsFiveM/StuntJumpsFiveM/StuntJumps.cs
@@ -8,119 +8,148 @@ namespace StuntJumpsFiveM
 {
     public class StuntJumps : BaseScript
     {
+
+        public List<int> stuntJumps = new List<int>();
+        public List<int> blips = new List<int>();
+
         public StuntJumps()
         {
+            // Blip label text
+            AddTextEntry("RADAR_STUNT_JUMP", "Stunt Jump");
 
-            // Setting up stats to make sure if the resource is restarted we DON'T re-add the stunt jumps, because the pool size is only 64, 
-            // and we add 50 stunt jumps, restarting the resource would break the client side stunt jumps pool size.
-            bool jumpsSetup = false;
-            StatGetBool((uint)GetHashKey("MP1_DEFAULT_STATS_SET"), ref jumpsSetup, -1);
+            // Enable blips if convar is set.
+            if (GetConvar("enable_stunt_jump_blips", "false").ToLower() == "true")
+            {
+                EnableBlips();
+            }
+
+            // Enable stunt jumps by default, but they can be disabled so that they can only be triggered via a command.
+            if (GetConvar("enable_stunt_jumps", "true").ToLower() == "true")
+            {
+                EnableStuntJumps();
+            }
+        }
+
+        /// <summary>
+        /// Automatically disable the stunt jumps if the player disconnects from the server.
+        /// </summary>
+        /// <param name="resourceName"></param>
+        [EventHandler("onResourceStop")]
+        public void OnResourceStop(string resourceName)
+        {
+            if (resourceName == GetCurrentResourceName())
+            {
+                DisableStuntJumps();
+            }
+        }
+
+        [EventHandler("stuntjumps:enableJumps")]
+        public void EnableStuntJumps()
+        {
+            // Make sure there are no stunt jumps set beforehand.
+            DisableStuntJumps();
 
             foreach (var stuntJump in JsonConvert.DeserializeObject<Dictionary<string, List<dynamic>>>(Properties.Resources.stuntjumps))
             {
-                if (stuntJump.Key == "angledStuntJumps")
+                foreach (var jump in stuntJump.Value)
                 {
-                    foreach (var jump in stuntJump.Value)
+                    if (stuntJump.Key == "angledStuntJumps")
                     {
-                        // don't add them if they're already previously setup
-                        if (!jumpsSetup)
-                        {
-                            AddStuntJumpAngled(
-                                (float)jump["jumpPos"]["start"]["X"],
-                                (float)jump["jumpPos"]["start"]["Y"],
-                                (float)jump["jumpPos"]["start"]["Z"],
-                                (float)jump["jumpPos"]["end"]["X"],
-                                (float)jump["jumpPos"]["end"]["Y"],
-                                (float)jump["jumpPos"]["end"]["Z"],
-                                (float)jump["jumpPos"]["radius"],
-                                (float)jump["landingPos"]["start"]["X"],
-                                (float)jump["landingPos"]["start"]["Y"],
-                                (float)jump["landingPos"]["start"]["Z"],
-                                (float)jump["landingPos"]["end"]["X"],
-                                (float)jump["landingPos"]["end"]["Y"],
-                                (float)jump["landingPos"]["end"]["Z"],
-                                (float)jump["landingPos"]["radius"],
-                                (float)jump["camPos"]["X"],
-                                (float)jump["camPos"]["Y"],
-                                (float)jump["camPos"]["Z"],
-                                150,
-                                0,
-                                0
-                            );
-                        }
-                        // add blip if enabled
-                        if (GetConvar("enable_stunt_jump_blips", "false").ToLower() == "true")
-                        {
-                            int blip = AddBlipForCoord((float)jump["jumpPos"]["start"]["X"], (float)jump["jumpPos"]["start"]["Y"], (float)jump["jumpPos"]["start"]["Z"]);
-                            SetBlipColour(blip, 0);
-                            BeginTextCommandSetBlipName("STRING");
-                            AddTextComponentSubstringPlayerName("Stunt Jump");
-                            EndTextCommandSetBlipName(blip);
-                            SetBlipAsShortRange(blip, true);
-                        }
-
+                        stuntJumps.Add(AddStuntJumpAngled(
+                            (float)jump["jumpPos"]["start"]["X"],
+                            (float)jump["jumpPos"]["start"]["Y"],
+                            (float)jump["jumpPos"]["start"]["Z"],
+                            (float)jump["jumpPos"]["end"]["X"],
+                            (float)jump["jumpPos"]["end"]["Y"],
+                            (float)jump["jumpPos"]["end"]["Z"],
+                            (float)jump["jumpPos"]["radius"],
+                            (float)jump["landingPos"]["start"]["X"],
+                            (float)jump["landingPos"]["start"]["Y"],
+                            (float)jump["landingPos"]["start"]["Z"],
+                            (float)jump["landingPos"]["end"]["X"],
+                            (float)jump["landingPos"]["end"]["Y"],
+                            (float)jump["landingPos"]["end"]["Z"],
+                            (float)jump["landingPos"]["radius"],
+                            (float)jump["camPos"]["X"],
+                            (float)jump["camPos"]["Y"],
+                            (float)jump["camPos"]["Z"],
+                            150,
+                            0,
+                            0
+                        ));
                     }
-                }
-                else
-                {
-                    foreach (var jump in stuntJump.Value)
+                    else
                     {
-                        // don't add them if they're already previously setup
-                        if (!jumpsSetup)
-                        {
-                            AddStuntJump(
-                                (float)jump["jumpPos"]["start"]["X"],
-                                (float)jump["jumpPos"]["start"]["Y"],
-                                (float)jump["jumpPos"]["start"]["Z"],
-                                (float)jump["jumpPos"]["end"]["X"],
-                                (float)jump["jumpPos"]["end"]["Y"],
-                                (float)jump["jumpPos"]["end"]["Z"],
-                                (float)jump["landingPos"]["start"]["X"],
-                                (float)jump["landingPos"]["start"]["Y"],
-                                (float)jump["landingPos"]["start"]["Z"],
-                                (float)jump["landingPos"]["end"]["X"],
-                                (float)jump["landingPos"]["end"]["Y"],
-                                (float)jump["landingPos"]["end"]["Z"],
-                                (float)jump["camPos"]["X"],
-                                (float)jump["camPos"]["Y"],
-                                (float)jump["camPos"]["Z"],
-                                150,
-                                0,
-                                0
-                            );
-                        }
-                        // add blip if enabled
-                        if (GetConvar("enable_stunt_jump_blips", "false").ToLower() == "true")
-                        {
-                            int blip = AddBlipForCoord((float)jump["jumpPos"]["start"]["X"], (float)jump["jumpPos"]["start"]["Y"], (float)jump["jumpPos"]["start"]["Z"]);
-                            SetBlipColour(blip, 0);
-                            BeginTextCommandSetBlipName("STRING");
-                            AddTextComponentSubstringPlayerName("Stunt Jump");
-                            EndTextCommandSetBlipName(blip);
-                            SetBlipAsShortRange(blip, true);
-                        }
+                        stuntJumps.Add(AddStuntJump(
+                            (float)jump["jumpPos"]["start"]["X"],
+                            (float)jump["jumpPos"]["start"]["Y"],
+                            (float)jump["jumpPos"]["start"]["Z"],
+                            (float)jump["jumpPos"]["end"]["X"],
+                            (float)jump["jumpPos"]["end"]["Y"],
+                            (float)jump["jumpPos"]["end"]["Z"],
+                            (float)jump["landingPos"]["start"]["X"],
+                            (float)jump["landingPos"]["start"]["Y"],
+                            (float)jump["landingPos"]["start"]["Z"],
+                            (float)jump["landingPos"]["end"]["X"],
+                            (float)jump["landingPos"]["end"]["Y"],
+                            (float)jump["landingPos"]["end"]["Z"],
+                            (float)jump["camPos"]["X"],
+                            (float)jump["camPos"]["Y"],
+                            (float)jump["camPos"]["Z"],
+                            150,
+                            0,
+                            0
+                        ));
                     }
                 }
             }
+        }
 
-            StatSetBool((uint)GetHashKey("MP1_DEFAULT_STATS_SET"), true, true);
-
-            EventHandlers.Add("onResourceStop", new Action<string>((resname) =>
+        [EventHandler("stuntjumps:disableJumps")]
+        public void DisableStuntJumps()
+        {
+            foreach (int jump in stuntJumps)
             {
-                if (resname == GetCurrentResourceName())
+                DeleteStuntJump(jump);
+            }
+            stuntJumps.Clear();
+            DisableBlips();
+        }
+
+        [EventHandler("stuntjumps:disableBlips")]
+        public void DisableBlips()
+        {
+            foreach (int blip in blips)
+            {
+                int b = blip;
+                if (DoesBlipExist(b))
                 {
-                    // reset the stat if the resource is being stopped because the player disconnected from the server
-                    // otherwise do nothing because the stunt jumps probably still exist.
-                    // This is a very hacky method, and does cause for some problems, but none that are too critical.
-                    // Just some 'visual' issues to do with keeping track of completed jumps. 
-                    // (things like 99 remaining stunt jumps etc will appear in banner message when using a jump after re-joining a server without game reboot)
-                    if (!NetworkIsSessionActive())
-                    {
-                        StatSetBool((uint)GetHashKey("MP1_DEFAULT_STATS_SET"), false, true);
-                        Debug.WriteLine($"[{GetCurrentResourceName()}] Resetting stunt jumps stat to allow them to be recreated on the next server join.");
-                    }
+                    RemoveBlip(ref b);
                 }
-            }));
+            }
+            blips.Clear();
+        }
+
+        [EventHandler("stuntjumps:enableBlips")]
+        public void EnableBlips()
+        {
+            // Remove existing blips first.
+            DisableBlips();
+
+            foreach (var stuntJump in JsonConvert.DeserializeObject<Dictionary<string, List<dynamic>>>(Properties.Resources.stuntjumps))
+            {
+                foreach (var jump in stuntJump.Value)
+                {
+                    // Add blip
+                    int blip = AddBlipForCoord((float)jump["jumpPos"]["start"]["X"], (float)jump["jumpPos"]["start"]["Y"], (float)jump["jumpPos"]["start"]["Z"]);
+                    SetBlipSprite(blip, 488);
+                    SetBlipColour(blip, 6);
+                    BeginTextCommandSetBlipName("RADAR_STUNT_JUMP");
+                    EndTextCommandSetBlipName(blip);
+                    SetBlipAsShortRange(blip, true);
+                    blips.Add(blip);
+                }
+            }
         }
     }
 }
